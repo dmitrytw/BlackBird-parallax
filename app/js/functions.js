9d8179d8c5c4235c01e35cc3cc7a333c7edc8c1b
dmitrytw <dmitry.tw@gmail.com>
2015-06-01 16:30:13 +0300

    Styled clothes pics and added animation

--- app/js/functions.js
+++ app/js/functions.js
@@ -12,4 +12,13 @@ $(window).scroll(function(){
 	$('.fore-bird').css({
 		'transform': 'translate(0px, -'+ wScroll / 40 + '%)'
 	});
+
+	if(wScroll > $('.clothes-pics').offset().top - $(window).height() / 1.2) {
+		$('.clothes-pics figure').each(function(i){
+
+			setTimeout(function(){
+				$('.clothes-pics figure').eq(i).addClass('is-showing');
+			}, 150 * (i+1));
+		});
+	}
 });
